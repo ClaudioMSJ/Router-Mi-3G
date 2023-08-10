aec675b57434153a1ece2a81ccea355e4cd2fd77
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2023-08-10 15:25:00 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,12 +1,37 @@
 #!/bin/sh
-
-
 echo ----- OPKG UPDATE -----
 opkg update
 
 sleep 5
 echo
+echo ----- INSTALL ADGUARD HOME -----
+opkg remove dnsmasq odhcpd-ipv6only
+rm /etc/config/dhcp
+opkg install sudo ca-certificates ca-bundle curl wget wget-ssl tar unzip bind-tools
+curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -c edge
+echo 'Sucess Install AdGuardHome.'
+
+sleep 5
+echo
 echo ----- DISABLE IPV6 -----
+uci set 'network.lan.ipv6=0'
+uci set 'network.wan.ipv6=0'
+uci set 'dhcp.lan.dhcpv6=disabled'
+/etc/init.d/odhcpd disable
+uci commit
+
+uci -q delete dhcp.lan.dhcpv6
+uci -q delete dhcp.lan.ra
+uci commit dhcp
+/etc/init.d/odhcpd restart
+
+uci set network.lan.delegate="0"
+uci commit network
+/etc/init.d/network restart
+
+/etc/init.d/odhcpd disable
+/etc/init.d/odhcpd stop
+
 uci -q delete network.globals.ula_prefix
 uci commit network
 /etc/init.d/network restart
@@ -17,15 +42,6 @@ echo ----- ENABLE FIREWALL HW -----
 uci set firewall.cfg01e63d.flow_offloading='1'
 uci set firewall.cfg01e63d.flow_offloading_hw='1'
 uci commit firewall
-
-sleep 5
-echo
-echo ----- INSTALL ADGUARD HOME -----
-opkg remove dnsmasq odhcpd-ipv6only
-rm /etc/config/dhcp
-opkg install sudo ca-certificates ca-bundle curl wget wget-ssl tar unzip bind-tools
-curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -c edge
-echo 'Sucess Install AdGuardHome.'
 
 sleep 5
 echo
