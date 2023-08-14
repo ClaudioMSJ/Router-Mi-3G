2d1ed4946211e9d7be68eae3a74383bb22768f5d
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2023-08-14 10:16:30 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-echo
-echo ----- TIMEZONE SÃO PAULO -----
-uci del system.ntp.enabled
-uci del system.ntp.enable_server
-uci set system.cfg01e48a.zonename='America/Sao Paulo'
-uci set system.cfg01e48a.timezone='<-03>3'
-uci set system.cfg01e48a.log_proto='udp'
-uci set system.cfg01e48a.conloglevel='8'
-uci set system.cfg01e48a.cronloglevel='5'
-uci commit system
-
-sleep 5
-echo
-echo ----- TASK REBOOT -----
-/etc/init.d/cron enable
-echo '00 06 * * * reboot' >> /etc/crontabs/root
-/etc/init.d/cron start
-
 echo ----- UPDATE -----
 opkg update
 
@@ -56,12 +38,7 @@ uci commit network
 sleep 5
 echo
 echo ----- REMOVE DNSMASQ -----
-opkg remove dnsmasq odhcpd-ipv6only
-opkg install odhcpd
-uci -q delete dhcp.@dnsmasq[0]
-uci set dhcp.lan.dhcpv4="server"
-uci set dhcp.odhcpd.maindhcp="1"
-uci commit dhcp
+opkg remove dnsmasq odhcpd odhcpd-ipv6only
 
 sleep 5
 echo
@@ -71,4 +48,3 @@ uci -q delete network.wan.dns
 uci add_list network.wan.dns="1.1.1.1"
 uci add_list network.wan.dns="1.0.0.1"
 uci commit network
-reboot
