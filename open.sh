750c73393c385f493191c45413a0bf021d83ffe8
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2024-11-29 16:10:15 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,51 +1,73 @@
 #!/bin/sh
 
-echo ----- UPDATE -----
-opkg update
-
-sleep 5
-echo
-echo ----- DISABLE IPV6 -----
+# Desabilitar IPV6
 uci set 'network.lan.ipv6=0'
 uci set 'network.wan.ipv6=0'
 uci set 'dhcp.lan.dhcpv6=disabled'
-uci commit
-
 uci -q delete dhcp.lan.dhcpv6
 uci -q delete dhcp.lan.ra
-uci commit dhcp
-
 uci set network.lan.delegate="0"
-uci commit network
-
 uci -q delete network.globals.ula_prefix
-uci commit network
 
-sleep 5
-echo
-echo ----- REMOVE DNSMASQ -----
-opkg remove dnsmasq odhcpd odhcpd-ipv6only
+# Desabilitar Led Azul
+uci add system led
+uci set system.@led[-1].name='Blue'
+uci set system.@led[-1].sysfs='blue:status'
+uci set system.@led[-1].trigger='none'
+uci set system.@led[-1].default='0'
 
-sleep 5
-echo
-echo ----- ENABLE FIREWALL HW -----
-uci set firewall.cfg01e63d.flow_offloading='1'
-uci set firewall.cfg01e63d.flow_offloading_hw='1'
-uci commit firewall
+# Horário
+uci set system.@system[0].zonename='America/Sao Paulo'
+uci set system.@system[0].timezone='<-03>3'
 
-sleep 5
-echo
-echo ----- INSTALL ADGUARD HOME -----
-opkg install curl
-opkg install sudo
-curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
-echo 'Sucess Install AdGuardHome.'
+# Ativar Hardware e Software Offloading
+uci set firewall.@defaults[0].flow_offloading='1'
+uci set firewall.@defaults[0].flow_offloading_hw='1'
 
-sleep 5
-echo
-echo ----- SET CLOUDFLARE WAN DNS -----
+# Desabilitar Allow-Ping
+uci set firewall.@rule[1].enabled=0
+
+# Desabilitar DNS ISP
 uci set network.wan.peerdns="0"
 uci -q delete network.wan.dns
 uci add_list network.wan.dns="1.1.1.1"
 uci add_list network.wan.dns="1.0.0.1"
-uci commit network
+
+# Camera GI - IP Fixo
+uci add dhcp host
+uci set dhcp.@host[-1].name="CameraGi"
+uci set dhcp.@host[-1].dns='1'
+uci set dhcp.@host[-1].mac="40:ED:00:28:25:3E"
+uci set dhcp.@host[-1].ip="192.168.1.250"
+
+# DNS Cloudflare
+while uci -q delete https-dns-proxy.@https-dns-proxy[0]; do :; done
+uci set https-dns-proxy.dns="https-dns-proxy"
+uci set https-dns-proxy.dns.bootstrap_dns="1.1.1.1,1.0.0.1"
+uci set https-dns-proxy.dns.resolver_url="https://cloudflare-dns.com/dns-query"
+uci set https-dns-proxy.dns.listen_addr="127.0.0.1"
+uci set https-dns-proxy.dns.listen_port="5053"
+
+#Adblock Lean
+uclient-fetch https://raw.githubusercontent.com/lynxthecat/adblock-lean/main/adblock-lean -O /etc/init.d/adblock-lean
+sh /etc/init.d/adblock-lean setup
+
+# Salvar Configs
+uci commit
+
+# Ao Iniciar
+rm /etc/rc.local
+echo 'sleep 30 && sh /root/adblock.sh
+sleep 120 && sync | echo 3 > /proc/sys/vm/drop_caches
+exit 0' >> /etc/rc.local
+
+echo '#!/bin/bash
+while ! ping -c 1 -W 1 8.8.8.8 > /dev/null 2>&1; do
+    echo "Waiting for 8.8.8.8 - network interface might be down..."
+    sleep 10
+done
+service adblock-lean restart
+echo "Adblock Service Manually Reloaded"' >> /root/adblock.sh
+chmod +x /root/adblock.sh
+
+reboot
