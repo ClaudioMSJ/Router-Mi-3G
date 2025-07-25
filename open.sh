93f9fe57faa1ebccd5f6a7917240ff6327d7e695
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2025-07-24 22:30:13 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -29,23 +29,31 @@ uci set firewall.@rule[1].enabled=0
 
 # Desabilitar DNS ISP
 uci set network.wan.peerdns='0'
-# uci set network.wan.dns='1.1.1.1 1.0.0.1'
+uci set network.wan.dns='1.1.1.1 1.0.0.1'
 
 # DOH Configs
-uci -q delete https-dns-proxy.@https-dns-proxy[0]
-uci set https-dns-proxy.cloudflare_doh="https-dns-proxy"
-uci set https-dns-proxy.cloudflare_doh.listen_addr="127.0.0.1"
-uci set https-dns-proxy.cloudflare_doh.listen_port="5053"
-uci add_list https-dns-proxy.cloudflare_doh.resolver_url="https://cloudflare-dns.com/dns-query"
-uci add_list https-dns-proxy.cloudflare_doh.resolver_url="https://1.1.1.1/dns-query"
-uci set https-dns-proxy.cloudflare_doh.bootstrap_dns="1.1.1.1,1.0.0.1"
-uci set https-dns-proxy.cloudflare_doh.force_dns='1'
-uci set https-dns-proxy.cloudflare_doh.enabled='1'
+while uci -q delete https-dns-proxy.@https-dns-proxy[0]; do :; done
+uci set https-dns-proxy.dns="https-dns-proxy"
+uci set https-dns-proxy.dns.bootstrap_dns="1.1.1.1,1.0.0.1"
+uci set https-dns-proxy.dns.resolver_url="https://cloudflare-dns.com/dns-query"
+uci set https-dns-proxy.dns.listen_addr="127.0.0.1"
+uci set https-dns-proxy.dns.listen_port="5053"
 
+# Dnsmasq Config
 uci set dhcp.@dnsmasq[0].noresolv='1'
-uci set dhcp.@dnsmasq[0].strictorder='1'
 uci -q delete dhcp.@dnsmasq[0].server
 uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5053'
+
+# DNS Firewall Rule
+uci -q del firewall.dns_int
+uci set firewall.dns_int="redirect"
+uci set firewall.dns_int.name="Intercept-DNS"
+uci set firewall.dns_int.family="any"
+uci set firewall.dns_int.proto="tcp udp"
+uci set firewall.dns_int.src="lan"
+uci set firewall.dns_int.src_dport="53"
+uci set firewall.dns_int.dest_port="5053" # Port your DoH proxy is listening on
+uci set firewall.dns_int.target="DNAT"
 
 #Adblock Lean
 uclient-fetch https://raw.githubusercontent.com/lynxthecat/adblock-lean/master/abl-install.sh -O /tmp/abl-install.sh
