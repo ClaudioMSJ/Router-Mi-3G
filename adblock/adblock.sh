66656efc64c26f01d3b4cffdb59403807b8e3655
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2025-07-31 16:28:46 -0300

    Update adblock.sh

--- adblock/adblock.sh
+++ adblock/adblock.sh
@@ -2,5 +2,5 @@ wget https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/pro.mi
 wget https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/pro.mini.txt -o /etc/dnsmasq.conf   2>/dev/null ; echo ok 
 
 uci set dhcp.@dnsmasq[0].addnhosts='/tmp/adblock_hosts'
-uci  commit dhcp
+uci commit dhcp
 /etc/init.d/dnsmasq restart 
