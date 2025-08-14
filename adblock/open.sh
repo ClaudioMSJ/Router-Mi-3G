bbb8cc4a3eb808de833e9d9adfaedb2f8de4f4c9
ClaudioMSJ <50308248+ClaudioMSJ@users.noreply.github.com>
2025-08-13 23:55:41 -0300

    Update open.sh

--- adblock/open.sh
+++ adblock/open.sh
@@ -80,6 +80,7 @@ while ! ping -c 1 -W 1 8.8.8.8 > /dev/null 2>&1; do
     echo "Waiting for 8.8.8.8 - network interface might be down..."
 done
 wget -q https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/dnsmasq_small.txt -O /etc/dnsmasq.conf
+sed -i '/^[[:space:]]*#/d;/^[[:space:]]*$/d' /etc/dnsmasq.conf
 /etc/init.d/dnsmasq restart ' >> /root/adblock.sh
 chmod +x /root/adblock.sh
 
